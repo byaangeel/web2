1d6570e68cf56c9ac82529ab2815727fb96e6f52
byaangeel <161104656+byaangeel@users.noreply.github.com>
2024-07-01 01:13:16 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,14 +6,3 @@ function toggleMenu() {
         sideMenu.style.width = '250px';
     }
 }
-// Inicializar Swiper para el slider de noticias
-var swiper = new Swiper(".swiper-container", {
-    loop: true, // Repetir el slider al llegar al final
-    autoplay: {
-        delay: 8000, // Intervalo de cambio de diapositivas (en milisegundos)
-    },
-    pagination: {
-        el: ".swiper-pagination", // Selector del contenedor de paginación
-        clickable: true, // Permitir hacer clic en los puntos de paginación para navegar
-    },
-});
